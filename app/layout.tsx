054db7fe9d50bbd6b6632b9e0584c5840ba3708c
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-05-01 02:09:37 +0530

    adding authprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthPovider from "@/context/AuthProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter( { subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthPovider>
+         <body className={inter.className}>{children}</body>
+      </AuthPovider>
     </html>
   );
 }
