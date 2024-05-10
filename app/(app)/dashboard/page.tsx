18aa4b2e47e3abe8c140d85baf6577a3b8b87657
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-05-11 03:52:11 +0530

    updates

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
+import MessageCard from "@/components/MessageCard"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { Switch } from "@/components/ui/switch"
 import { useToast } from "@/components/ui/use-toast"
 import { Message } from "@/models/User"
 import { acceptMessagesSchema } from "@/schemas/acceptMessageSchema"
 import { ApiResponce } from "@/types/apiResponce"
 import { zodResolver } from "@hookform/resolvers/zod"
 import axios, { AxiosError } from "axios"
+import { Loader2, RefreshCcw } from "lucide-react"
 import { useSession } from "next-auth/react"
 import { useCallback, useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
@@ -95,11 +100,76 @@ const Dashboard = () => {
    })
   }
  }
+const baseUrl =`${window.location.protocol}//${window.location.host}`
+const profileUrl ='${baseUrl}/u/${useranme}'
+const copyToClipboard = () => {
+  navigator.clipboard.writeText(profileUrl)
+  toast({
+    title: 'URL copied',
+    description: "Profile URL has been copied"
+  })
+}
  if(!session || !session.user){
   return <div>Please login</div>
  }
  return (
-    <div>Dashboard</div>
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">
+       User Dashboard
+      </h1>
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy your Unique Link</h2>{' '}
+        <div className="flex items-center">
+          <input 
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+           />
+           <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+      <div className="mb-4">
+        <Switch
+        {...register('acceptMessages')}
+        checked={acceptMessages}
+        onCheckedChange={handleSwitchChamge}
+        disabled={isSwitchLoading}
+        >
+          <span className="ml-2">
+            Accept Message: {acceptMessages ? 'On' : 'Off'}
+          </span>
+        </Switch>
+      </div>
+      <Separator/>
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true)
+        }}>
+          { isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin"/>
+           ) : (
+            <RefreshCcw className="h-4 w-4"/>
+           )   }
+        </Button>
+
+       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {message.length > 0 ? (
+          message.map((message, index) => (
+            <MessageCard
+            key={message._id}
+            message={message}
+            onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <p>No messages to dishplay</p>
+        )}
+       </div>
+    </div>
   )
 }
 
--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,48 @@
-
+"use client"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import messages from "@/message.json";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen ">
-    <h1>hello</h1>
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-4 py-12">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl">Dive into the word of anonymous Conversation</h1>
+        <p className="mt-3 md:mt-4 text-base md:text-lg">Explore Mystery Message - Where your identify remains a secret</p>
+      </section>
+      <Carousel
+        plugins={[Autoplay({ delay: 2000 })]}
+        className="w-full max-w-xs">
+        <CarouselContent>
+          {
+            messages.map((message, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardHeader>
+                      {message.title}
+                    </CardHeader>
+                    <CardContent className="text-lg aspect-square items-center justify-center p-6">
+                      <samp className="text-lg font-semibold">
+                        {message.content}
+                      </samp>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))
+          }
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </main>
   );
 }
